24c40e9b4ffc664bfe963c8553e552c20ed3c8c4
AnjaliDubey2607 <anjali.d@cisinlabs.com>
2022-12-15 17:13:52 +0530

    up

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,7 +5,7 @@ class Category < ApplicationRecord
     has_many :subordinates, class_name: 'Category', foreign_key: 'parent_id', dependent: :destroy
     belongs_to :parent, class_name: 'Category', optional: true
 
-    scope :category,->{ Category.where(parent_id:nil)}
+    scope :category,->{ Category.where(parent_id: nil)}
     scope :category_name, ->{ Category.where.not(id: Category.where.not(parent_id:nil).pluck(:parent_id) )}
     scope :sub_category, ->(params){Category.where(parent_id:params)}
     scope :find_category, ->(attributes,params){Category.find_by(attributes => params)}
