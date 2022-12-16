ff7060a0e0d7e8f6843bad78ae5c3d61ce62e5c9
AnjaliDubey2607 <anjali.d@cisinlabs.com>
2022-12-16 13:02:03 +0530

    up

--- db/migrate/20221216072614_add_des_to_product.rb
+++ db/migrate/20221216072614_add_des_to_product.rb
@@ -1,4 +1,4 @@
-class AddDescriptionToProduct < ActiveRecord::Migration[7.0]
+class AddDesToProduct < ActiveRecord::Migration[7.0]
   def change
     add_column :products, :description, :string
   end
